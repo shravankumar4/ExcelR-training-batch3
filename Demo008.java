6fccaa87fd537e98353a54a57df079588badf637
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-03 17:07:00 +0530

    Add files via upload

---
+++ Demo008.java
@@ -0,0 +1,17 @@
+public class Demo008 {
+    public static void main(String[] args) {
+        do { 
+        System.out.println("Welcome to Mallareddy University");
+        }
+        System.out.println("Enter Details"); 
+        String name;
+        int age;
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        age = sc.nextInt();
+            
+        }
+        while{
+            System.out.println("you are succesgully logged in");
+        }
+    }
