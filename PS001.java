bd55e94f4bc142bb74adc4415f2b5c5835fb0750
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-07 10:57:46 +0530

    Add files via upload

---
+++ PS001.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class PS001 {
+    int id;
+    String firstName;
+    String lastName;
+    String address;
+    Employee(){
+        this(0, null, null, null);
+    }
+    Employee(int id, String firstName, String lastName, String address){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName = lastName;
+        this.address=address;
+    }
+    String getFullName(){
+        return firstName+" "+lastName;
+    }
+}
+class PS001{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int id = 100;
+        String firstName = "Shravan"; 
+        String lastName = "Kumar";
+        String address = "Hyderabad";
+        Employee emp1 = new Employee();
+        System.out.println("Default constructor:\nFull name : "+emp1.getFullName());
+        Employee emp = new Employee(id, firstName, lastName, address);
+        System.out.println("Parameterized constructoer:\nFull name : "+emp.getFullName());
+
+    }
+}
