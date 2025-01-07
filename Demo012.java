9f0981ce11a17280946466fea9eba205808c44b6
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-07 11:33:06 +0530

    Demo012.java

--- Demo012.java
+++ Demo012.java
@@ -9,7 +9,7 @@ class CircleAreaDemo1 {
 	}
 
 }
-public class PS002 {
+public class Demo012 {
     public static void main(String[] args) {
         CircleAreaDemo1 obj = new CircleAreaDemo1();
         obj.Area();
