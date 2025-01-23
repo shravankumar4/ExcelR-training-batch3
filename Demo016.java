f9a5d9e2d43b53e9b02b4f5ba8a12b40175714c4
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-23 21:25:31 +0530

    Palindrome

---
+++ Demo016.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Demo016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = new StringBuilder(input).reverse().toString();
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
