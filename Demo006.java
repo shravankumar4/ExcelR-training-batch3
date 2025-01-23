289d6b12a7bbbba43320400c5612346a9021c77a
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-23 21:16:54 +0530

    while loop

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "Ashwitha";
+        String pwd = "Ashu_006";
+        while((name == "Ashwitha") && (pwd == "Ashu_006")) {
+            System.out.println("Happy Birthday");
+            break;
+        }
+    }
+}
