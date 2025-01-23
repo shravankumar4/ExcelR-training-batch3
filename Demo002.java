9a5187e225c7741dc81c8ee93acef4ebd475b69e
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-23 21:12:00 +0530

    finding greatest number using if else

---
+++ Demo002.java
@@ -0,0 +1,12 @@
+class Demo002 {
+    public static void main(String args[]) {
+        int num1 = 12;
+        int num2 = 18;
+        if(num1 > num2){
+            System.out.println("num1 is greater ");
+        }
+        else{
+            System.out.println("num2 is greater ");
+        }
+    }
+}
