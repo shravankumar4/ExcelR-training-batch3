ab803c2e421e291a6bbef85e2430f2c997a93687
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-25 12:29:18 +0530

    swapping two numbers

---
+++ Demo045.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Demo045 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+    }
+}
