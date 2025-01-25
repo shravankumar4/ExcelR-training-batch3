42b253e45afcb71f19fcae2c4139d171e2871685
shravankumar4 <sravandarshanala4@gmail.com>
2025-01-25 12:25:32 +0530

    to swap 2 numbers with 3rd variable

---
+++ Demo044.java
@@ -0,0 +1,12 @@
+class Demo044{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+        }
+}
