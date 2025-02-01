399dfb4b81f0f3a36fdd5b09f90680f147402326
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-01 12:43:33 +0530

    Finding greatest number

---
+++ pdemo002.java
@@ -0,0 +1,12 @@
+public class pdemo002 {
+    public static void main(String args[]){
+        int num1 = 12;
+        int num2 = 15;
+        if (num1 < num2){
+            System.out.println("num1 is greater ");
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+        }
+    }
