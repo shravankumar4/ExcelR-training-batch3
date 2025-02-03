721a439027bb5b522bc5accb2ad9c0d3fe07fa86
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-03 20:56:34 +0530

    sum of cubes of a digit of a number

---
+++ Demo056.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Demo056{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+        while(num>0){
+            int digit = num % 10;
+            sum += digit*digit*digit;
+            num /= 10;
+        }
+        System.out.println("Sum of cubes of digits: "+sum);
+    }
+}
