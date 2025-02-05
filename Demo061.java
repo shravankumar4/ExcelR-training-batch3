6efa4ea53789af1cefe59566fc898ed437651bed
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-05 12:12:34 +0530

    to print 1 to 10 prime numbers

---
+++ Demo061.java
@@ -0,0 +1,16 @@
+public class Demo061 {
+    public static void main(String[] args) {
+        for (int num = 2; num <= 10; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
