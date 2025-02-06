6ab297aba78f1f202264f3ef746751c782361724
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-06 20:12:16 +0530

    sum of all the elements of an array

---
+++ Demo069.java
@@ -0,0 +1,11 @@
+class Demo069{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            sum+=arr[i];
+            arr[i]++;
+        }
+        System.out.println("Sum of elements in an array is "+sum);
+    }
+}
