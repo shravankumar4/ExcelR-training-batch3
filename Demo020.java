80dc84cadebf141374a4384f3e462b4714d7b647
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-09 22:26:10 +0530

    Fibonnaci series

--- Demo020.java
+++ Demo020.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
-public class Demo019 {
+public class Demo020 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        double number = scanner.nextDouble();
-        long rounded = Math.round(number);
-        System.out.println("Rounded figure: " + rounded);
-    }
+        int n = 10, a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
 }
