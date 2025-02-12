7f05826b4c611a38b3e2c9b02f659b7aa4bea68f
shravankumar4 <sravandarshanala4@gmail.com>
2025-02-12 12:51:52 +0530

    to find maximum number in an array

---
+++ Demo075.java
@@ -0,0 +1,10 @@
+class Demo075{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,12,7};
+        for(int i = 0 ;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                System.out.println(arr[i]);
+            }
+}
+}
+}
